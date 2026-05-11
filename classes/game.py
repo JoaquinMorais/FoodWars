8ef41d3783706ee3ad1fd628178dd6e9e7caf969
JoaquinMorais <joaquinmorais2005@gmail.com>
2026-05-10 23:55:35 -0300

    cambiada forma de pelear por la comida

--- classes/game.py
+++ classes/game.py
@@ -34,28 +34,52 @@ class Game():
     def simulate_one_day(self):
         individues_actives = self.individues.copy()
         self.individues = []
-        
-        #pelear por la comida
-        for tree in range(self.trees):
-            if len(individues_actives) == 0:
-                break 
-            elif len(individues_actives) != 1:
-                ind1, ind2 = random.sample(individues_actives, 2)
-                individues_actives.remove(ind1)
-                individues_actives.remove(ind2)
+        random.shuffle(individues_actives)
 
+        final_trees = {x:[None,None] for x in range(self.trees)}
+        free_trees = [x for x in range(self.trees)]
+
+        #pelear por la comida
+        print(individues_actives)
+        while individues_actives:
+            individue = individues_actives.pop()
+            print(individue)
+
+            if len(free_trees) == 0:
+                break
+
+            tree = random.choice(free_trees)
+
+            if final_trees[tree][0] is None:
+                final_trees[tree][0] = individue
+
+            else:
+                final_trees[tree][1] = individue
+                free_trees.remove(tree)
+
+
+        for tree in final_trees:
+
+            ind1 = final_trees[tree][0]
+            ind2 = final_trees[tree][1]
+
+            if ind1 is None:
+                continue
+
+            elif ind2 is None:
+                ind1.food += self.fruits
+                self.individues += [ind1]
+                print(f'{ind1} eat all')
+
+            else:
                 result = self.fight(ind1.action(ind2), ind2.action(ind1))
                 print(result)
                 ind1.food += result[0]
                 ind2.food += result[1]
-
                 self.individues += [ind1, ind2]
-                
-            else:
-                ind = individues_actives[0]
-                individues_actives.remove(ind)
-                ind.food += self.fruits
-                self.individues += [ind]
+        self.individues += individues_actives
 
-        print(self.individues)
+        
+
+                
 
